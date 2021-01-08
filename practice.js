bc9e845bd77b57d3c39e231d5f93557663195818
Jon Squire <johnycake27@gmail.com>
2021-01-08 09:35:08 -0700

    initial commit

--- practice.js
+++ practice.js
@@ -103,7 +103,7 @@ var user2 = {
 */
 
 user2.name = 'Bryan G. Smith'
-user2.email = 'bryan.smith@devmounta.in'
+user2.email = 'bryan.smith@dgit evmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
